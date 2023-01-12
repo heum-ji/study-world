a26514bc713bfa207b803c5d58f72b0bc56ddff4
jiheum <dragon2009t@gmail.com>
2023-01-13 00:05:08 +0900

    [Add] 프로젝트 생성

---
+++ HiJava/src/hijava/practice/HelloJava.java
@@ -0,0 +1,11 @@
+package hijava.practice;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        say("Hi~");
+    }
+
+    public static void say(String msg) {
+        System.out.println(msg);
+    }
+}
