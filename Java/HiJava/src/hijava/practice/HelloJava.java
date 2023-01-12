5cbd44bd7f775d4ea7e9ac10bca943ec49402a37
jiheum <dragon2009t@gmail.com>
2023-01-13 00:37:06 +0900

    docs: javadoc 주석 추가

--- Java/HiJava/src/hijava/practice/HelloJava.java
+++ Java/HiJava/src/hijava/practice/HelloJava.java
@@ -1,8 +1,17 @@
 package hijava.practice;
 
+/**
+ * 첫번째 자바 실습
+ *
+ * @author Heum
+ */
 public class HelloJava {
     public static void main(String[] args) {
-        say("Hi~");
+        // HelloJava에게 say라는 명령을 내림!
+        HelloJava.say("Hi~");
+
+        // JVM이 스스로 화면에 출력
+        System.out.println("Hello Java!!");
     }
 
     public static void say(String msg) {
