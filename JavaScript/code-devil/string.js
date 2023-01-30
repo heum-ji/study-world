749c4424985eb31fa6fc8feffd991a084b337e0e
jiheum <dragon2009t@gmail.com>
2023-01-31 00:41:49 +0900

    feat: 자바스크립트 기초 강좌 - 자료형

---
+++ JavaScript/code-devil/string.js
@@ -0,0 +1,19 @@
+/*
+*  변수
+*  변하지 않는 값 : const (상수) - 가급적 대문자 사용
+*  변할 수 있는 값 : let (변수)
+* */
+const name = "Mike";
+const age = 30;
+
+const name1 = "Mike";
+const name2 = 'Mike';
+const name3 = `Mike`;
+
+const message = "I'm a boy.";
+const message2 = 'I\'am a boy.';
+
+const message3 = `My name is ${name}`;
+
+const message4 = `나는 ${30+1}살 입니다.`;
+console.log(message4);
