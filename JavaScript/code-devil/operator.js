5ed32b88d32dd13749607f97e3f620f6817ddccb
jiheum <dragon2009t@gmail.com>
2023-02-02 23:20:17 +0900

    feat: 자바스크립트 기초 강좌 - 기본 연산자

---
+++ JavaScript/code-devil/operator.js
@@ -0,0 +1,14 @@
+/*
+* 연산자 (Operator)
+*
+* 제곱 = **
+* 연산자 줄여쓰기 가능 ex) +=, *=, %=, Etc
+* 전위 : 선 증가 후 대입
+* 후위 : 선 대입 후 증가
+* */
+
+let num = 10;
+//num++;
+let result = num++;
+
+console.log(result);
