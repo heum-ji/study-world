1bc021b1bf4a726442028ebad8b61cc7fa52078a
jiheum <dragon2009t@gmail.com>
2023-02-02 23:37:02 +0900

    feat: 자바스크립트 기초 강좌 - 비교 연산자, 조건문(if, else)

---
+++ JavaScript/code-devil/conditional-statement.js
@@ -0,0 +1,18 @@
+/*
+* 조건문
+* if, else, else if
+* */
+
+// 19살이면 수능 잘 치세요 라는 문구를 보여주세요
+const age = 19;
+
+if (age > 19) {
+    console.log('환영 합니다.');
+} else if (age === 19) {
+    console.log('수능 잘 치세요.')
+} else {
+    console.log('안녕히 가세요.');
+
+}
+
+console.log('--------------------');
--- JavaScript/code-devil/operator.js
+++ JavaScript/code-devil/operator.js
@@ -5,10 +5,28 @@
 * 연산자 줄여쓰기 가능 ex) +=, *=, %=, Etc
 * 전위 : 선 증가 후 대입
 * 후위 : 선 대입 후 증가
+*
+* 비교 연산자
+*
 * */
 
-let num = 10;
+//let num = 10;
 //num++;
-let result = num++;
+// let result = num++;
+//
+// console.log(result);
 
-console.log(result);
+
+// 비교연산자
+console.log(10 > 5);
+console.log(10 == 5); // 동등 연산자
+console.log(10 != 5);
+
+// 동등 연산자
+// == : 값만 비교
+// 일치 연산자
+// === : 타입과 값 모두 비교
+const a = 1;
+const b = "1";
+console.log(a == b); // true
+console.log(a === b); // false
