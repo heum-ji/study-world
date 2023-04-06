bcb3effa290fc9e24cd7e9357faf784ee15bebda
jiheum <dragon2009t@gmail.com>
2023-04-07 00:52:20 +0900

    feat: 회원 관리 예제 - 백엔드 개발
    - 비즈니스 요구사항 정리
    - 회원 도메인과 리포지토리 만들기
    - 회원 리포지토리 테스트 케이스 작성
    - 회원 서비스 개발
    - 회원 서비스 테스트

---
+++ Spring/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,42 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    // 실무에서는 동시성 문제로 고려해야 함
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); // NULL 이여도 리턴하기 위해 ofNullable 사용
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        // findAny : 1개라도 찾으면 리턴 (NULL인 경우도 리턴됨)
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
---
+++ Spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,63 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        // DI : 직접 생성하지 않고 외부에서 생성해줌
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     *
+     * @param member 가입할 회원
+     *
+     * @return 가입된 회원의 id
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 중복 회원 검증
+     *
+     * @param member 중복 검증할 회원
+     */
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    /**
+     * 전체 회원 조회
+     *
+     * @return 조회된 전체 회원 리스트
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 1명 조회
+     *
+     * @param memberId 조회할 회원 ID
+     *
+     * @return 조회된 회원
+     */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
